7dae0eedfb5fba6a73620a075e96ef53ae52b16e
guyleaf <ychhua1@gmail.com>
2021-04-10 15:12:02 +0800

    Update trainer.py

--- data/dataset/ovsd_bbc_task.py
+++ data/dataset/ovsd_bbc_task.py
@@ -72,7 +72,6 @@ class OVSDBBCDataset(Dataset):
             # END: label_length
             encoder[feature_length+1:] = float(-1)
             
-            print(labels)
             # START: 0
             decoder[0] = float(1)
             decoder[1:label_length+1] = features[labels]
@@ -95,10 +94,8 @@ class OVSDBBCDataset(Dataset):
         with h5py.File(file_path, 'r') as f:
             if f.attrs["dataset_name"] == "BBC":
                 self.data_cache[file_path] = [f.attrs["dataset_name"], np.array(f['/features'][()]), np.array(f['/labels/annotator_0'][()])]
-                print(f['/labels/annotator_0'][()])
             elif f.attrs["dataset_name"] == "OVSD":
                 self.data_cache[file_path] = [f.attrs["dataset_name"], np.array(f['/features'][()]), np.array(f['/labels'][()])]
-                # print(f['/labels'][()])
 
     def get_name(self, index):
         return self.data_cache[self.file_paths[index]][0]
