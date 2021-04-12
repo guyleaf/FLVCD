256561b3794659bf4e4fb76c8abecb5c01178f8b
guyleaf <ychhua1@gmail.com>
2021-04-13 00:52:39 +0800

    Add masking out attention to self

--- model/attention.py
+++ model/attention.py
@@ -5,17 +5,21 @@ import torch
 
 class Attention(nn.Module):
     def forward(self, query, key, value, mask):
-        model_dim = query.size(-1)
+        model_dim, model_len = query.size(-1), query.size(-2)
 
         query *= float(model_dim) ** -0.5
 
         # Calculating Attention Score
         scores = torch.matmul(query, key.transpose(-1, -2))
-        
-        # Reference: https://discuss.pytorch.org/t/runtimeerror-value-cannot-be-converted-to-type-at-half-without-overflow-1e-30/109768
-        _MASKING_VALUE = -1e+30 if scores.dtype == torch.float32 else -1e+4
 
-        # Fill -1e9 when mask == 1
+        # Reference: https://discuss.pytorch.org/t/runtimeerror-value-cannot-be-converted-to-type-at-half-without-overflow-1e-30/109768
+        _MASKING_VALUE = -1e+30 if scores.dtype == torch.float32 else -5e+4
+
+        # Mask out attention to self
+        i = torch.arange(model_len)
+        scores[:, :, i, i] = _MASKING_VALUE
+
+        # Fill _MASKING_VALUE when mask == 1
         if mask is not None:
             scores = scores.masked_fill(mask, _MASKING_VALUE)
 
@@ -57,7 +61,7 @@ class MultiHeadAttention(nn.Module):
 
         # Concat H distributed Attention
         # attention = attention.view(batch_size, query.size(1), _key.size(1))
-        sum_value = sum_value.transpose(1, 2).contiguous().view(batch_size, -1, self.d_model)
+        sum_value = sum_value.view(batch_size, -1, self.d_model).transpose(1, 2).contiguous()
 
         # Return attention and value
         return sum_value
